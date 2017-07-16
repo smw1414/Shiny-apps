c203c62ff192a26279cc23930e79332439591918
smw1414 <wsm147@outlook.com>
2017-07-16 23:21:49 +0800

    add merge bam function

--- app.R
+++ app.R
@@ -12,6 +12,7 @@ library(shinydashboard)
 annovar_folder<-basename(system("ls -d /home/wsm/RA_files/app/annovar/* ",intern = T))
 header <- dashboardHeader()
 fastq_folder<-basename(system("ls -d /home/wsm/RA_files/app/cat_fastq/* ",intern = T))
+bammergefd<-basename(system("ls -d /home/wsm/RA_files/app/bam_merge/* ",intern = T))
 sidebar <-   dashboardSidebar(
   useShinyjs(),
   sidebarMenu(
@@ -19,9 +20,13 @@ sidebar <-   dashboardSidebar(
     selectInput("annovar_folder_sel", "Folder",
                 choices = c(unique(annovar_folder)), multiple=F, selectize=TRUE,selected =  NULL,
                 width = '98%'),
-    menuItem("merage fastq", tabName = "cat_fastq" ,icon = icon("list-alt")),
+    menuItem("Merage fastq", tabName = "cat_fastq" ,icon = icon("list-alt")),
     selectInput("fastq_folder_sel", "Folder",
                 choices = c(unique(fastq_folder)), multiple=F, selectize=TRUE,selected =  NULL,
+                width = '98%'),
+    menuItem("Merge bam", tabName = "merge_bam" ,icon = icon("list-alt")),
+    selectInput("bammerge_folder_sel", "Folder",
+                choices = c(unique(bammergefd)), multiple=F, selectize=TRUE,selected =  NULL,
                 width = '98%')
 
   )
@@ -71,6 +76,24 @@ body <-   dashboardBody(
               )
               )
 
+     ),
+     tabItem(tabName = "merge_bam",
+             fluidRow( column(6,
+                              h3(paste0("1. Create a folder under RA_files\\app\\merge_bam \n")),
+                              h3(paste0("2. Copy bam files to the folder you just created\n")),
+                              h3(paste0("3. Select the the folder you created from sidebar menu,\n or press F5 if the folder was not existed in the drop down menu ")),
+                              h3(paste0("4. Fill the new merged bam below\n")),
+                              h3(paste0("5. Click the button: Merge bam \n")),
+                              h3(paste0("6. Collect merged bam file in the folder you just created, the progress indicator was showed under the Merge fastq button\n")),
+                             
+                              textInput("newbamname", label = h3("New file name (Do not add .bam)"), value = "Enter_new_name_here"),
+                              verbatimTextOutput("file_preview_mergebam"),
+                              #                 # verbatimTextOutput("runapp"),
+                              actionButton("apply_mergebam", "Merge bam")
+                              # textOutput("outlog")
+             )
+             )
+             
      )
    )
 )
@@ -83,6 +106,7 @@ server <- shinyServer(function(input, output, session) {
   
 ######## aanovar 
   # vcf_list <- reactive({})
+  ##### list folders
   observe({
     x<-basename(system("ls -d /home/wsm/RA_files/app/annovar/* ",intern = T))
     updateSelectInput(session, "annovar_folder_sel",
@@ -101,6 +125,16 @@ server <- shinyServer(function(input, output, session) {
 
   })
   
+  observe({
+    x<-basename(system("ls -d /home/wsm/RA_files/app/bam_merge/* ",intern = T))
+    updateSelectInput(session, "bammerge_folder_sel",
+                      label = paste("Folder"),
+                      choices = x,
+                      selected = tail(x, 1))
+    
+  })
+  
+  ##### preview files in the given folder
   output$file_preview <- renderPrint({
     cat("First 10 files were showed\n")
     cat(input$annovar_folder_sel)
@@ -119,6 +153,17 @@ server <- shinyServer(function(input, output, session) {
     cat(files)
   })
   
+  output$file_preview_mergebam <- renderPrint({
+    cat("First 10 files were showed\n")
+    cat(input$bammerge_folder_sel)
+    cat("\n")
+    files<-system(paste0("ls -d /home/wsm/RA_files/app/bam_merge/",input$bammerge_folder_sel,"/*.bam "),intern = T)
+    files<-basename(files[1:10][!is.na(files[1:10])])
+    cat(files)
+  })
+  
+  
+  #### APP function
   runapp_annovar<- eventReactive(input$apply_anno, {
     files<-system(paste0("ls /home/wsm/RA_files/app/annovar/",input$annovar_folder_sel,"/*.vcf"),intern = T)
     out=paste0("/home/wsm/RA_files/app/annovar/",input$annovar_folder_sel)
@@ -145,7 +190,7 @@ server <- shinyServer(function(input, output, session) {
      }
    })
 
-  
+   system(paste0("chmod 777 -R /home/wsm/RA_files/app/"))
    enable_act_but()
   # cat(paste0(aa))
    
@@ -173,16 +218,38 @@ server <- shinyServer(function(input, output, session) {
         message(paste0(i," "))
         system(paste(run_app,files[i],sep = " "))
         # Increment the progress bar, and update the detail text.
-        incProgress(1/n, detail = paste("File", i))
+        incProgress(1/n, detail = paste(files[i]," done"))
       }
     })
     
-    
+    system(paste0("chmod 777 -R /home/wsm/RA_files/app/"))
     enable_act_but()
     # cat(paste0(aa))
     
   })
   
+  runapp_mergebam<- eventReactive(input$apply_mergebam, {
+
+    run_app <- "/home/wsm/bam/scripts/bammerge/bammerge.sh"
+    ##aa
+  
+    disable_act_but()
+    #Sys.sleep(2)
+    message("merge bam")
+    
+    message(paste0("folder",input$bammerge_folder_sel))
+    message(paste0("file: ",input$newbamname))
+    wd=paste("/home/wsm/RA_files/app/bam_merge/",input$bammerge_folder_sel,sep = "/")
+   
+     message(paste(run_app,"-i",wd,"-o",input$newbamname,sep = " "))
+    system(paste(run_app,"-i",wd,"-o",input$newbamname,sep = " "))  
+    
+    system(paste0("chmod 777 -R /home/wsm/RA_files/app/"))
+    enable_act_but()
+
+  })
+  
+
   
   # output$runapp = renderPrint({
   #   runapp_annovar()  
@@ -202,6 +269,11 @@ server <- shinyServer(function(input, output, session) {
   observeEvent(input$apply_fastq, {
     runapp_catfastq()
   })
+  
+  observeEvent(input$apply_mergebam, {
+    runapp_mergebam()
+  })
+  
   # observe({
   #   if (input$close > 0) stopApp() # stop shiny
   # })
@@ -217,7 +289,12 @@ server <- shinyServer(function(input, output, session) {
     shinyjs::enable("apply_fastq")
     shinyjs::enable("fastq_folder_sel")
     shinyjs::enable("fastq_radio")
+    shinyjs::enable("apply_mergebam")
+    shinyjs::enable("newbamname")
+    shinyjs::enable("bammerge_folder_sel")
     
+    
+  
   }
   disable_act_but<-function(){
    # message("disabling but\n")
@@ -229,6 +306,9 @@ server <- shinyServer(function(input, output, session) {
     shinyjs::disable("apply_fastq")
     shinyjs::disable("fastq_folder_sel")
     shinyjs::disable("fastq_radio")
+    shinyjs::disable("apply_mergebam")
+    shinyjs::disable("newbamname")
+    shinyjs::disable("bammerge_folder_sel")
   }
   session$allowReconnect(TRUE)
 }
